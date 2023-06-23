fc180225616fcf6f35c8eb76bec80afa14349470
김효종 <hue.kim@samsung.com>
2023-06-23 13:53:21 +0900

    add assert to test

--- src/reverseEngSample/Refactor.java
+++ src/reverseEngSample/Refactor.java
@@ -1,5 +1,7 @@
 package reverseEngSample;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class Refactor {
@@ -10,6 +12,14 @@ public class Refactor {
 		me.addRental(new Rental(10, new Movie("Avengers1", Movie.REGULAR)));
 		me.addRental(new Rental(10, new Movie("Ttanic", Movie.CLASSIC)));
 		me.addRental(new Rental(10, new Movie("Gozilla", Movie.RELEASE)));
-		System.out.println(me.statement());
+		
+		String expected = new String("Statement\n"
+				+ "Avengers1\t14000\n"
+				+ "Ttanic\t12000\n"
+				+ "Gozilla\t30000\n"
+				+ "Total\t56000\n"
+				+ "Bonus Point:\t4\n");
+		
+		assertEquals(expected, me.statement());
 	}
 }
