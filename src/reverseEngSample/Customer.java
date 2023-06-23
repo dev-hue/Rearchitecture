05e201c8f9af83dae13aaab649e920c18d237eb8
김효종 <hue.kim@samsung.com>
2023-06-23 14:00:37 +0900

    step 2

--- src/reverseEngSample/Customer.java
+++ src/reverseEngSample/Customer.java
@@ -18,40 +18,56 @@ public class Customer {
 		return this.name;
 	}
 
-	public String statement() {
-		String rentalList = "Statement\n";
-		int totalAmount = 0;
-		int bonusPoints = 0;
+    public String statement() {
+        StringBuilder rentalList = new StringBuilder("Statement\n");
+        int totalAmount = 0;
+        int bonusPoints = 0;
 
-		for(Rental rent:rentals) {
-			int lineAmount = 0;
+        for (Rental rental : rentals) {
+            int lineAmount = calculateRentalAmount(rental);
+            bonusPoints += calculateBonusPoints(rental);
 
-			switch(rent.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				lineAmount += 2000;
-				if(rent.getDaysRented() > 2) {
-					lineAmount += (rent.getDaysRented() - 2) * 1500;
-				}
-				break;
-			case Movie.CLASSIC:
-				lineAmount += 1500;
-				if(rent.getDaysRented() > 3) {
-					lineAmount += (rent.getDaysRented() - 3) * 1500;
-				}
-				break;
-			case Movie.RELEASE:
-				lineAmount += (rent.getDaysRented() * 3000);
-				break;
-			}
-			bonusPoints++;
-			if((rent.getMovie().getPriceCode() == Movie.RELEASE)
-					&& (rent.getDaysRented() > 1)) bonusPoints++;
+            rentalList.append(rental.getMovie().getName()).append("\t").append(lineAmount).append("\n");
+            totalAmount += lineAmount;
+        }
 
-			rentalList += rent.getMovie().getName() + "\t" + String.valueOf(lineAmount) + "\n";			
-			totalAmount += lineAmount;
-		}
-		rentalList += "Total\t" + String.valueOf(totalAmount) + "\n";
-		rentalList += "Bonus Point:\t" + String.valueOf(bonusPoints) + "\n";
-		return rentalList;
-	}
+        rentalList.append("Total\t").append(totalAmount).append("\n");
+        rentalList.append("Bonus Points:\t").append(bonusPoints).append("\n");
+
+        return rentalList.toString();
+    }
+
+    private int calculateRentalAmount(Rental rental) {
+        int lineAmount = 0;
+
+        switch (rental.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                lineAmount += 2000;
+                if (rental.getDaysRented() > 2) {
+                    lineAmount += (rental.getDaysRented() - 2) * 1500;
+                }
+                break;
+            case Movie.CLASSIC:
+                lineAmount += 1500;
+                if (rental.getDaysRented() > 3) {
+                    lineAmount += (rental.getDaysRented() - 3) * 1500;
+                }
+                break;
+            case Movie.RELEASE:
+                lineAmount += (rental.getDaysRented() * 3000);
+                break;
+        }
+
+        return lineAmount;
+    }
+
+    private int calculateBonusPoints(Rental rental) {
+        int bonusPoints = 1;
+
+        if (rental.getMovie().getPriceCode() == Movie.RELEASE && rental.getDaysRented() > 1) {
+            bonusPoints++;
+        }
+
+        return bonusPoints;
+    }
 }
